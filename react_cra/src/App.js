72be0624738bbcefe42ce843b14b98b138b374c5
HBSPS <wiyeong79@naver.com>
2023-05-28 15:30:21 +0900

    [6.4] Cleanup

--- react_cra/src/App.js
+++ react_cra/src/App.js
@@ -1,26 +1,25 @@
 import { useState, useEffect } from 'react';
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello() {
+  const effectFn = () => {
+    console.log('Created')
+    return destroyFn
+  }
+  const destroyFn = () => console.log('Destroyed');
+  
+  useEffect(effectFn, [])
+  
+  return <h1>Hello</h1>
+}
 
-  useEffect(() => {
-    console.log('RUN FOR once');
-  }, []);
-  useEffect(() => {
-    console.log('RUN FOR counter');
-  }, [counter]);
-  useEffect(() => {
-    console.log('RUN FOR keyword');
-  }, [keyword]);
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
   return (
     <div>
-      <input value={keyword} onChange={onChange} type='text' placeholder='Search here...' />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
