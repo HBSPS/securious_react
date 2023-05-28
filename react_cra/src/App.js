b3f97fb44cc7de8ca2bcbc49836837c57fff6b83
HBSPS <wiyeong79@naver.com>
2023-05-28 14:17:06 +0900

    [6.2] useEffect Deps

--- react_cra/src/App.js
+++ react_cra/src/App.js
@@ -2,10 +2,23 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    console.log('RUN FOR once');
+  }, []);
+  useEffect(() => {
+    console.log('RUN FOR counter');
+  }, [counter]);
+  useEffect(() => {
+    console.log('RUN FOR keyword');
+  }, [keyword]);
 
   return (
     <div>
+      <input value={keyword} onChange={onChange} type='text' placeholder='Search here...' />
       <h1>{counter}</h1>
       <button onClick={onClick}>click</button>
     </div>
