4749331a23b5caa5a09eb7801e0a0d3063d50585
HBSPS <wiyeong79@naver.com>
2023-05-28 14:03:47 +0900

    [6.1] useEffect

--- react_cra/src/App.js
+++ react_cra/src/App.js
@@ -1,11 +1,13 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+
   return (
     <div>
-      <h1 className={styles.title}>Hello World!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click</button>
     </div>
   );
 }
