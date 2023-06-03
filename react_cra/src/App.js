078d663da4681c31d29d4523143a6fd7f9c80569
HBSPS <wiyeong79@naver.com>
2023-06-03 14:57:41 +0900

    [7.1] To Do List (2)

--- react_cra/src/App.js
+++ react_cra/src/App.js
@@ -13,7 +13,6 @@ function App() {
     setToDo("");
     setToDos((prevArr) => [toDo, ...prevArr]);
   };
-  console.log(toDos)
 
   return (
     <div>
@@ -27,6 +26,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
